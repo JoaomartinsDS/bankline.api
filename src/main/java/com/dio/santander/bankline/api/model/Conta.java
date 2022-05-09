5047d84efb6fa71a94d81b9f013e1bd5c4a8b3a8
joao <JoaobmDS@outlook.com>
2022-05-09 17:46:54 -0300

    Entidades mapeadas

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,5 +1,18 @@
 package com.dio.santander.bankline.api.model;
 
 public class Conta {
-
+	private Long numero;
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	private Double saldo;
 }
