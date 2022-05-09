f7c4789cb4083bbbc37e8eff313acd3344a2b8aa
joao <JoaobmDS@outlook.com>
2022-05-09 17:19:30 -0300

    Update Application.java

--- src/main/java/com/dio/santander/bankline/api/Application.java
+++ src/main/java/com/dio/santander/bankline/api/Application.java
@@ -2,7 +2,7 @@ package com.dio.santander.bankline.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Começando o projeto
 @SpringBootApplication
 public class Application {
 
